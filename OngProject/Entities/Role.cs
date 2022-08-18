de09bb080f5ec28db7dd224d1bfb1cb6b624af21
Luis J. Montes <luisjavm3@gmail.com>
2022-08-18 15:21:52 -0500

    Crea la entidad Role en la carpeta Entities usando DataAnnotations

---
+++ OngProject/Entities/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class Role
+    {
+        // Id is not defined yet.
+
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Description { get; set; }
+
+        // UpdatedAt - Timestamp
+        // It is allowed to be null
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
