82beabe621f749288010425df0009ad8182cfa2e
LucasLudu <lucaslud13@hotmail.com>
2022-08-18 21:44:04 -0300

    News entity is added

---
+++ OngProject/Entities/News.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class News
+    {
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(65535)]
+        public string Content { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Image { get; set; }
+
+        // Falta agregar Entidad Categoria
+        //public int CategoriaId { get; set; }
+        //public Category Category { get; set; }
+    }
+}
