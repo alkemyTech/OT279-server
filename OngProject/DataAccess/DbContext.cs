197e100d5b82208d9682f53c8301c0e64d33104d
Luis J. Montes <luisjavm3@gmail.com>
2022-08-19 09:41:02 -0500

    Install EntityFramewrok and get started with DbContext

--- OngProject/DataAccess/DbContext.cs
+++ OngProject/DataAccess/DbContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OngProject.DataAccess
 {
-    public class DbContext
+    public class DbContext : Microsoft.EntityFrameworkCore.DbContext
     {
+        public DbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+                
+        }
+
+
     }
 }
