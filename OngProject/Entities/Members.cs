6dfedaf6a52ca0a79585983d2050064002f2c122
Lucas <lucas_turza@hotmail.com.ar>
2022-08-19 19:21:30 -0300

    Members class created

---
+++ OngProject/Entities/Members.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class Members
+    {
+        //falta heredar de clase base Entity para obtener id, timestamps y softDeletes
+        [MaxLength(255)]
+        public string Name { get; set; }
+        [MaxLength(255)]
+        public string FacebookUrl { get; set; }
+        [MaxLength(255)]
+        public string InstagramUrl { get; set; }
+        [MaxLength(255)]
+        public string LinkedinUrl { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Image { get; set; }
+        [MaxLength(255)]
+        public string Description { get; set; }
+    }
+}
