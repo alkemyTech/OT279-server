dff7ccac4c08ae5da8b3209fc2f7c73a00a3285f
Lucas <lucas_turza@hotmail.com.ar>
2022-08-19 20:26:43 -0300

    Created members repository and service without logic

---
+++ OngProject/Core/Interfaces/IMembersBusiness.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace OngProject.Core.Interfaces
+{
+    public interface IMembersBusiness
+    {
+        public IEnumerable<Members> GetAll();
+        public Members GetById(int Id);
+        public void Insert(Members member);
+        public void Update(Members updatedMember);
+        public bool Delete(int Id);
+    }
+}
---
+++ OngProject/Repositories/Interfaces/IMembersRepository.cs
@@ -0,0 +1,15 @@
+﻿using OngProject.Entities;
+using System.Collections.Generic;
+
+namespace OngProject.Repositories.Interfaces
+{
+    public interface IMembersRepository
+    {
+        public IEnumerable<Members>GetAll();
+        public Members GetById(int Id);
+        public void Insert(Members member);
+        public void Update(Members updatedMember);
+        public bool Delete(int Id);
+
+    }
+}
