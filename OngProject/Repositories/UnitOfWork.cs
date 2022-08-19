9503aff85a4c2017ada51b8b519e9cad3fbec68c
Eze <Eze@DESKTOP-RQJN4SS>
2022-08-19 15:58:21 -0300

    Se agrego la clase de Entity, La interface de UnitOfWork y Repositoris y su respetiva implementacion

---
+++ OngProject/Repositories/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using OngProject.DataAccess;
+using OngProject.Repositories.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace OngProject.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly OngDbContext _context;
+
+        public UnitOfWork(OngDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Complete()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
