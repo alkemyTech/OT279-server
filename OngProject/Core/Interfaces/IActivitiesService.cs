eae70c8681f56a8c492275c297a81a22436c4c31
Javier Solis <javiersolis.unaj@gmail.com>
2022-08-20 17:34:09 -0300

    Activities Entity & IActivitiesService

---
+++ OngProject/Core/Interfaces/IActivitiesService.cs
@@ -0,0 +1,18 @@
+﻿using OngProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Interfaces
+{
+    interface IActivitiesService
+    {
+        public List<Activities> GetAllActivities();
+        public Activities GetActivitiesById(int id);
+        public Activities CreateActivities(ActivitiesDTO activities);
+        public bool RemoveActivities(int id);
+        public Activities UpdateActivities(Activities activities);
+ 
+    }
+}
---
+++ OngProject/Entities/Activities.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Entities
+{
+    public class Activities
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+
+        [MaxLength(65535)]
+        public string Content { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Image { get; set; }
+        
+        public DateTime DeletedAt { get; set; }
+
+    }
+}
