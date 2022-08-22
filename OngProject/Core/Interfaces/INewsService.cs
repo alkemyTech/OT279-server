bda64f93f40c23d058d42c54bae646a77bb89074
LucasLudu <lucaslud13@hotmail.com>
2022-08-22 14:30:18 -0300

    News Service

---
+++ OngProject/Core/Interfaces/INewsService.cs
@@ -0,0 +1,15 @@
+﻿using OngProject.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Interfaces
+{
+    public interface INewsService
+    {
+        Task<List<News>> GetAllNews();
+        Task<News> GetNewsById(int id);
+        Task<News> CreateNews(NewsDTO news);
+        public Task<bool> RemoveNews(int id);
+        public Task<Activities> UpdateNews(int id, NewsDTO news);
+    }
+}
