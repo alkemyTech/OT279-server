0d74efee143ea80d7f08bc5df5f77d9c514e1470
Facundo <cabrera.facundo990@gmail.com>
2022-08-22 10:48:42 -0300

    CategoriesRepository created, using Repository pattern

--- OngProject/DataAccess/OngDbContext.cs
+++ OngProject/DataAccess/OngDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OngProject.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,6 @@ namespace OngProject.DataAccess
 
         }
 
+        public DbSet<Category> Categories { get; set; }
     }
 }
