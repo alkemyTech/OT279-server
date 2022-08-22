aac91fce6c73438f5b1e6531be462b06ad673350
LucasLudu <lucaslud13@hotmail.com>
2022-08-22 13:38:15 -0300

    Added News Repository

--- OngProject/Repositories/UnitOfWork.cs
+++ OngProject/Repositories/UnitOfWork.cs
@@ -1,4 +1,5 @@
 ﻿using OngProject.DataAccess;
+using OngProject.Entities;
 using OngProject.Repositories.Interfaces;
 using System;
 using System.Threading.Tasks;
@@ -9,9 +10,12 @@ namespace OngProject.Repositories
     {
         private readonly OngDbContext _context;
 
+        public IRepository<News> NewsRepository { get; private set; }
+
         public UnitOfWork(OngDbContext context)
         {
             _context = context;
+            NewsRepository = new Repository<News>(context);
         }
 
         public async Task<int> Complete()
