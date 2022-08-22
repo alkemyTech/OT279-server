e7484572f84880f179d871b8ee9a46b382596ae7
Agustin Flores <agustinflores1505@gmail.com>
2022-08-22 14:07:05 -0300

    Entity User fixed

--- OngProject/Entities/User.cs
+++ OngProject/Entities/User.cs
@@ -5,77 +5,35 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace OngProject.Entities
 {
     [Index(nameof(Email), IsUnique = true)]
-    public class User 
+    public class User : Entity
     {
-        /*
-            TODO: Heredar de clase base Entity para obtener ID, softDeleted y timestamp
-         */
-
-        private string _firstname;
-        private string _lastname;
-        private string _email;
-        private string _password;
-        private string _photo;
-        private int _roleId;
-
         [Required, MaxLength(255)]
         [Display(Name = "First_Name")]
-        public string FirstName
-        {
-            get { return _firstname; }
-            set { _firstname = value; }
-        }
+        public string FirstName { get; set; }
 
         [Required, MaxLength(255)]
         [Display(Name = "Last_Name")]
-        public string LastName
-        {
-            get { return _lastname; }
-            set { _lastname = value; }
-        }
+        public string LastName { get; set; }
+
 
         [Required, MaxLength(320)]
         [Display(Name = "Email")]
-        public string Email
-        {
-            get { return _email; }
-            set { _email = value; }
-        }
+        public string Email { get; set; }
 
         [Required, MaxLength(20)]
         [Display(Name = "Password")]
-        public string Password
-        {
-            get { return _password; }
-            set { _password = value; }
-        }
+        public string Password { get; set; }
+
 
         [MaxLength(255)]
         [Display(Name = "Photo")]
-        public string Photo
-        {
-            get { return _photo; }
-            set { _photo = value; }
-        }
+        public string Photo { get; set; }
 
         [ForeignKey("Role")]
-        public int RoleId
-        {
-            get { return _roleId; }
-            set { _roleId = value; }
-        }
+        public int RoleId { get; set; }
 
-        /*
-        TODO: agregar atributo "Role".
-        
-        private Role _role; 
-        
-        public Role Role
-        {
-            get { return _role; }
-            set { _role = value; }
-        }
-        */
+        //public Role Role { get; set; }
+
         private User() { }
 
         public User(string firstname, string lastname, string email, string password, string photo, int roleId)
