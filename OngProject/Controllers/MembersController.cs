060ba207d9ffbb488ae7a7c45529836b143d678c
Lucas <lucas_turza@hotmail.com.ar>
2022-08-22 15:01:14 -0300

    added members controller

--- OngProject/Controllers/MembersController.cs
+++ OngProject/Controllers/MembersController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OngProject.Core.Interfaces;
+using System.Threading.Tasks;
 
 namespace OngProject.Controllers
 {
@@ -7,5 +9,82 @@ namespace OngProject.Controllers
     [ApiController]
     public class MembersController : ControllerBase
     {
+        private readonly IMembersService _membersService;
+        public MembersController(IMembersService membersService)
+        {
+            _membersService = membersService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMembers()
+        {
+            var members = await _membersService.GetAllMembers();
+
+            if(members != null)
+            {
+                return Ok(members);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMember([FromBody] MemberDTO memberDTO)
+        {
+            var member = await _membersService.CreateMember(memberDTO);
+
+            if(member != null)
+            {
+                return Ok(member);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveMember(int Id)
+        {
+            bool member = await _membersService.DeleteMember(Id);
+            if (member)
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateMember(int Id, MemberDTO memberDTO)
+        {
+            var member = await _membersService.UpdateMember(Id, memberDTO);
+            if(member != null)
+            {
+                return Ok(member);
+            }
+            else
+            {
+                return NotFound(400);
+            }
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetMemberById(int Id)
+        {
+            var member = await _membersService.GetMemberById(Id);
+            if(member != null)
+            {
+                return Ok(member);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
     }
 }
