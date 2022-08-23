365539c5919eeb65ba3c4224c17ebb5f86464599
LucasLudu <lucaslud13@hotmail.com>
2022-08-23 16:31:12 -0300

    New Service

---
+++ OngProject/Core/Business/NewsService.cs
@@ -0,0 +1,35 @@
+﻿using OngProject.Core.Interfaces;
+using OngProject.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Business
+{
+    public class NewsService : INewsService
+    {
+        public Task<News> CreateNews(News news)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<News>> GetAllNews()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<News> GetNewsById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> RemoveNews(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<News> UpdateNews(int id, News news)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
