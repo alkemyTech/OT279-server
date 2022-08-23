96e4318ba1975f3ef1f439e89c802ebfa80a379c
Agustin Flores <agustinflores1505@gmail.com>
2022-08-23 14:39:57 -0300

    Entity Contact added

---
+++ OngProject/Entities/Contact.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class Contact : Entity
+    {
+        [Required, MaxLength(255)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required, MaxLength(20)]
+        [Display(Name = "Phone")]
+        public int Phone { get; set; }
+        
+        [Required, MaxLength(320)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required, MaxLength(500)]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+       
+        private Contact() { }
+
+        public Contact(string name, int phone, string email, string message)
+        {
+            Name = name;
+            Phone = phone;
+            Email = email;
+            Message = message;
+        }
+    }
+}
