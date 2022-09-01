cd305e69a6c10529496fceb3c57d8875e96147fb
LucasLudu <lucaslud13@hotmail.com>
2022-09-01 11:56:15 -0300

    Added CreateActivities

--- OngProject/Core/Interfaces/IActivitiesBusiness.cs
+++ OngProject/Core/Interfaces/IActivitiesBusiness.cs
@@ -1,4 +1,5 @@
-﻿using OngProject.Entities;
+﻿using OngProject.Core.Models.DTOs;
+using OngProject.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,7 @@ namespace OngProject.Core.Interfaces
     {
         public Task<List<Activities>> GetAllActivities();
         public Task<Activities> GetActivitiesById(int id);
-        public Task<Activities> CreateActivities(Activities activities);
+        public Task<ActivitiesDisplayDTO> CreateActivities(ActivitiesCreateDTO activitiesDto);
         public Task<bool> RemoveActivities(int id);
         public Task<Activities> UpdateActivities(int id, Activities activities);
  
