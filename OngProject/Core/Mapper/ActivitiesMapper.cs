27558e21f0966ec110b628c8950ac1a7d2685033
LucasLudu <lucaslud13@hotmail.com>
2022-09-01 12:01:31 -0300

    Added Activities DTOs and Mapper

---
+++ OngProject/Core/Mapper/ActivitiesMapper.cs
@@ -0,0 +1,29 @@
+﻿using OngProject.Core.Models.DTOs;
+using OngProject.Entities;
+
+namespace OngProject.Core.Mapper
+{
+    public class ActivitiesMapper
+    {
+        public Activities FromActivitiesCreateDtoToActivities(ActivitiesCreateDTO activitiesDto)
+        {
+            var activities = new Activities
+            {
+                Name = activitiesDto.Name,
+                Content = activitiesDto.Content,
+            };
+            return activities;
+        }
+
+        public ActivitiesDisplayDTO FromActivitiesToActivitiesDisplayDto(Activities activities)
+        {
+            var dto = new ActivitiesDisplayDTO
+            {
+                Name = activities.Name,
+                Content = activities.Content,
+                Image = activities.Image
+            };
+            return dto;
+        }
+    }
+}
