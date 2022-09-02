cb58ccffd0ea2a6346b06105da7a28f01846874a
Edijosmen <edison.jm26@gmail.com>
2022-09-01 21:43:45 -0500

    Feature/US-53

---
+++ OngProject/Core/Mapper/NewsMapper.cs
@@ -0,0 +1,21 @@
+﻿using OngProject.Core.Models.DTOs.NewsDTO;
+using OngProject.Entities;
+
+namespace OngProject.Core.Mapper
+{
+    public class NewsMapper
+    {
+        public static GetNewsDto NewsToGetNewsDTO(News news)
+        {
+            GetNewsDto getNewsDtoDto = new()
+            {
+                Name = news.Name,
+                Content = news.Content,
+                Image = news.Image,
+                LastModified = news.LastModified
+
+            };
+            return getNewsDtoDto;
+        }
+    }
+}
