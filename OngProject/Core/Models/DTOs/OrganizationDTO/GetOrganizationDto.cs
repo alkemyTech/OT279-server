db437c7fd888c9eaf5928968329c238e4aa04df3
Agustin Flores <agustinflores1505@gmail.com>
2022-09-05 12:29:27 -0300

    Feature/US-69
    Agregue lista de Slides al DTO de Organization. 
    Modifique el service de organizacion para que tome las slides y retorne el DTO con estas.

--- OngProject/Core/Models/DTOs/OrganizationDTO/GetOrganizationDto.cs
+++ OngProject/Core/Models/DTOs/OrganizationDTO/GetOrganizationDto.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using OngProject.Entities;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OngProject.Core.Models.DTOs.OrganizationDTO
@@ -6,14 +8,28 @@ namespace OngProject.Core.Models.DTOs.OrganizationDTO
     public class GetOrganizationDto
     {
         public string Name { get; set; }
-
         public string Image { get; set; }
-
         public string Address { get; set; }
-
         public int? Phone { get; set; }
         public string FacebookUrl { get; set; }
         public string LinkedinUrl { get; set; }
         public string InstagramUrl { get; set; }
+        public List<SlideDTO> _listSlides { get; set; }
+
+        public GetOrganizationDto() { }
+
+        public GetOrganizationDto(Organization org)
+            :base()
+        {
+            _listSlides = new List<SlideDTO>();
+            Name = org.Name;
+            Image = org.Image;
+            Address = org.Address;
+            Phone = org.Phone;
+            FacebookUrl = org.FacebookUrl;
+            LinkedinUrl = org.LinkedinUrl;
+            InstagramUrl = org.InstagramUrl;
+        }
+
     }
 }
