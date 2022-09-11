ae76e870fb038462ed2ae889b371605f10a889d4
Ezequiel Caliguri <74575681+ezecaliguri@users.noreply.github.com>
2022-09-10 21:59:44 -0300

    Cambios en el DTO

--- OngProject/Core/Models/DTOs/PagedListDTO/PagedListDTO.cs
+++ OngProject/Core/Models/DTOs/PagedListDTO/PagedListDTO.cs
@@ -1,4 +1,4 @@
-﻿using OngProject.Core.Helper;
+using OngProject.Core.Helper;
 using OngProject.Entities;
 using System.Collections.Generic;
 
@@ -6,16 +6,22 @@ namespace OngProject.Core.Models.DTOs.PagedListDTO
 {
     public class PagedListDTO<T>
     {
-        public PagedListHelper<T> Values { get; set; }
+        
         public string PreviousPage { get; set; }
         public string NextPage { get; set; }
-
+        public int currentPage { get; set; }
+        public int totalPage { get; set; }
+        public int pageSize { get; set; }
+        public PagedListHelper<T> Values { get; set; }
+        
         public PagedListDTO(PagedListHelper<T> entity, string host, string path)
         {
-
+            pageSize = entity.PageSize;
+            totalPage = entity.TotalPage;
+            currentPage = entity.CurrentPage;            
+            PreviousPage = entity.HasPrevious ? $"https://{host}{path}?numberPage={entity.CurrentPage + 1}&quantityPage={entity.PageSize}" : null;
+            NextPage = entity.HasNext ? $"https://{host}{path}?numberPage={entity.CurrentPage + 1}&quantityPage={entity.PageSize}" : null;            
             Values = entity;
-            PreviousPage = entity.HasPrevious ? $"https://{host}{path}?numberPage={entity.CurrentPage - 1}&quantityPage={entity.PageSize}" : null;
-            NextPage = entity.HasNext ? $"https://{host}{path}?numberPage={entity.CurrentPage + 1}&quantityPage={entity.PageSize}" : null;
         }
     }
 }
