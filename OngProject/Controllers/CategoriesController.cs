40950fe10ab8132a0a88e4014bd9927f363a73cc
Lucas <lucas_turza@hotmail.com.ar>
2022-09-12 22:50:14 -0300

    added pagination to categories

--- OngProject/Controllers/CategoriesController.cs
+++ OngProject/Controllers/CategoriesController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using OngProject.Core.Helper;
 using OngProject.Core.Interfaces;
 using OngProject.Core.Models.DTOs.CategoriesDTO;
+using OngProject.Core.Models.DTOs.PagedListDTO;
 using OngProject.Entities;
 using System;
 using System.Threading.Tasks;
@@ -19,14 +21,34 @@ namespace OngProject.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllCategories()
+        //public async Task<IActionResult> GetAllCategories()
+        //{
+
+        //    var categories = await _service.GetAllCategories();
+
+        //    if (categories != null)
+        //    {
+        //        return Ok(categories);
+        //    }
+        //    else
+        //    {
+        //        return NotFound();
+        //    }
+
+        //}
+
+        public async Task<IActionResult> GetAllCategories([FromQuery(Name = "numberPage")] int numberPage = 1, [FromQuery(Name = "quantityPage")] int quantityPage = 10)
         {
+            var host = HttpContext.Request.Host.Value;
+            var path = HttpContext.Request.Path.Value;
 
-            var categories = await _service.GetAllCategories();
+            var categoryDTO = await _service.GetAllCategories();
 
-            if (categories != null)
+            if (categoryDTO != null)
             {
-                return Ok(categories);
+                PagedListHelper<GetNameCategoriesDTO> paged = PagedListHelper<GetNameCategoriesDTO>.Create(categoryDTO, numberPage, quantityPage);
+                PagedListDTO<GetNameCategoriesDTO> testList = new(paged, host, path);
+                return Ok(testList);
             }
             else
             {
--- OngProject/Core/Interfaces/ICategoriesBusiness.cs
+++ OngProject/Core/Interfaces/ICategoriesBusiness.cs
@@ -3,13 +3,14 @@ using OngProject.Core.Models.DTOs;
 using OngProject.Core.Models.DTOs.CategoriesDTO;
 using OngProject.Entities;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace OngProject.Core.Interfaces
 {
     public interface ICategoriesBusiness
     {
-        public Task<List<GetNameCategoriesDTO>> GetAllCategories();
+        public Task<IQueryable<GetNameCategoriesDTO>> GetAllCategories();
         public Task<GetCategoriesDTO> GetCategoryById(int id);
         public Task<GetCategoriesDTO> CreateCategory(CreateCategoriesDTO category);
         public Task<bool> RemoveCategory(int id);
