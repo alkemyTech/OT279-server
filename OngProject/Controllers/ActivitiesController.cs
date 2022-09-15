b263332904509172d482a3aeef3f67d383322da5
LucasLudu <lucaslud13@hotmail.com>
2022-09-15 20:57:43 -0300

    Added Activities Documentation

--- OngProject/Controllers/ActivitiesController.cs
+++ OngProject/Controllers/ActivitiesController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using OngProject.Core.Interfaces;
 using OngProject.Core.Mapper;
 using OngProject.Core.Models.DTOs.ActivitiesDTO;
-using OngProject.Entities;
 using System.Threading.Tasks;
 
 namespace OngProject.Controllers
@@ -20,7 +19,24 @@ namespace OngProject.Controllers
             _service = service;
         }
 
+        /// GET: activities
+        /// <summary>
+        ///     Gets all activities.
+        /// </summary>
+        /// <remarks>
+        ///     Gets information paged about the activities in the database.
+        /// </remarks>
+        /// <response code="200">OK. Returns activities information.</response>  
+        /// <response code="400">BadRequest. Invalid request received.</response> 
+        /// <response code="401">Unauthoried.</response> 
+        /// <response code="404">Not found. Server couldn't find the activities.</response> 
+        /// <response code="500">Internal Server Error.</response> 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetAllActivities()
         {
 
@@ -37,7 +53,22 @@ namespace OngProject.Controllers
 
         }
 
+        /// <summary>
+        ///     Creates a new Activity.
+        /// </summary>
+        /// <remarks>
+        ///     Adds a new activity in the database.
+        /// </remarks>
+        /// <param name="activitiesDTO">New Activity data transfer object.</param>
+        /// <response code="200">OK. Returns a result object along with the new activity information.</response>        
+        /// <response code="400">BadRequest. Activity could not be created.</response>    
+        /// <response code="401">Unauthorized. Invalid JWT Token or it wasn't provided.</response>    
+        /// <response code="500">Internal Server Error.</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> CreateActivities([FromQuery] ActivitiesCreateDTO activitiesDTO)
         {
 
@@ -54,7 +85,24 @@ namespace OngProject.Controllers
 
         }
 
-        [HttpDelete]
+        /// <summary>
+        ///     Deletes an activity.
+        /// </summary>
+        /// <remarks>
+        ///     Deletes an Activity in the database.
+        /// </remarks>
+        /// <param name="id">Id of the activity that'll be removed from the database</param>
+        /// <response code="200">OK. Returns a result object if the activity was successfully removed.</response>        
+        /// <response code="400">BadRequest. Activity could not be removed.</response>    
+        /// <response code="401">Unauthorized. Invalid JWT Token or it wasn't provided.</response>
+        /// <response code="404">Not found. Server couldn't find the activity with the id provided.</response> 
+        /// <response code="500">Internal Server Error.</response>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> RemoveActivities([FromQuery(Name = "id")] int id)
         {
 
@@ -71,7 +119,25 @@ namespace OngProject.Controllers
 
         }
 
-        [HttpPut]
+        /// <summary>
+        ///     Updates an activity.
+        /// </summary>
+        /// <remarks>
+        ///     Updates an activity in the database.
+        /// </remarks>
+        /// <param name="activitiesDTO">New value for the activity</param>
+        /// <param name="id">Id from activity for changes</param>
+        /// <response code="200">Ok. Return the new activity updated</response>
+        /// <response code="400">BadRequest. Activity could not be updated.</response>    
+        /// <response code="401">Unauthorized. Invalid JWT Token or it wasn't provided.</response>   
+        /// <response code="404">Not found. Server couldn't find the activity.</response> 
+        /// <response code="500">Internal Server Error</response>
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> UpdateActivities([FromQuery(Name = "id")] int id, [FromQuery] ActivitiesCreateDTO activitiesDTO)
         {
             var existActivityByID = await _service.GetActivitiesById(id);
@@ -88,7 +154,22 @@ namespace OngProject.Controllers
             }
         }
 
-        [HttpGet("id")]
+        /// <summary>
+        ///     Gets an activity information.
+        /// </summary>
+        /// <remarks>
+        ///     Gets information about the activity with the id provided.
+        /// </remarks>
+        /// <param name="id">Activity id that will be searched.</param>        
+        /// <response code="200">OK. Returns activity information.</response> 
+        /// <response code="400">BadRequest. Invalid request received.</response> 
+        /// <response code="404">Not found. Server couldn't find the activity.</response> 
+        /// <response code="500">Internal Server Error.</response> 
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetActivitiesById([FromQuery(Name = "id")] int id)
         {
 
