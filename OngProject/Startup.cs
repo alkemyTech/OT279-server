01b2d180bc42ba92cb3e266e2333444a02a4186c
Lucas <lucas_turza@hotmail.com.ar>
2022-09-16 16:07:36 -0300

    Added Documentation to testimonials

--- OngProject/Startup.cs
+++ OngProject/Startup.cs
@@ -95,7 +95,7 @@ namespace OngProject
                 c.AddSecurityDefinition("Bearer",
                     new OpenApiSecurityScheme
                     {
-                        Description = "Autenticación JWT (Bearer)",
+                        Description = "Autenticaciï¿½n JWT (Bearer)",
                         Type = SecuritySchemeType.Http,
                         Scheme = "bearer"
                     });
